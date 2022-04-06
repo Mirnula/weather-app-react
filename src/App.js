b17ce3768cce220fa4f7d8e3613afda0aab3ab52
Mirnula <95304071+Mirnula@users.noreply.github.com>
2022-04-06 23:17:35 +0200

    added link to git

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import WeatherSearch from './WeatherSearch';
 function App() {
   return (
     <div className="App">
+      <div className = "weatherApp">
  <h1>Weather App</h1>
       <WeatherSearch />
+    </div>
+   < a href='https://github.com/Mirnula/weather-app-react'>Open-source code </a> by Mirna Brizic
     </div>
   );
 }
